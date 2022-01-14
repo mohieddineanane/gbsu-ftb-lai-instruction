d5f9fd9f56131fbe83802e16b9a028fe73309e8a
Mohieddine Anane <mohieddine.anane@gmail.com>
2022-01-14 12:19:06 +0100

    test usecase 1 should return 1 ( success )

--- gbsuftblai-back/src/main/java/com/kata/gbsuftblai/services/GbsuFtbLaiService.java
+++ gbsuftblai-back/src/main/java/com/kata/gbsuftblai/services/GbsuFtbLaiService.java
@@ -11,7 +11,14 @@ public class GbsuFtbLaiService {
 	
 	
 	public ResultDto convertNumber(Integer inputNumber) {
+		
 		ResultDto resultDto = new ResultDto();
+		StringBuffer result = new StringBuffer();
+		
+		
+		result.append(String.valueOf(inputNumber));
+		resultDto.setResult(result.toString());
+		
 		return resultDto;
 	}
 
