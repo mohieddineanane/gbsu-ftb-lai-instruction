6f103a0b36f5c8105f138bae1d51c9ab86b133f8
Mohieddine Anane <mohieddine.anane@gmail.com>
2022-01-14 12:33:08 +0100

    test usecase 3 should return GbsuGbsu ( success )

--- gbsuftblai-back/src/main/java/com/kata/gbsuftblai/services/GbsuFtbLaiService.java
+++ gbsuftblai-back/src/main/java/com/kata/gbsuftblai/services/GbsuFtbLaiService.java
@@ -3,6 +3,7 @@ package com.kata.gbsuftblai.services;
 
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Lists;
 import com.kata.gbsuftblai.dto.ResultDto;
 
 
@@ -15,8 +16,25 @@ public class GbsuFtbLaiService {
 		ResultDto resultDto = new ResultDto();
 		StringBuffer result = new StringBuffer();
 		
+		if (inputNumber % 3 == 0) {
+			result.append("Gbsu");
+		}
+	
+		String number = String.valueOf(inputNumber);
+
+		for (Character ch : Lists.charactersOf(number)) {
+			if (String.valueOf(ch).equals("3")) {
+				result.append("Gbsu");
+			}
+		}
 		
-		result.append(String.valueOf(inputNumber));
+		//	result.append(String.valueOf(inputNumber));
+        if(result.toString().isEmpty())
+        {
+        	result.append(String.valueOf(inputNumber));
+        }
+
+
 		resultDto.setResult(result.toString());
 		
 		return resultDto;
